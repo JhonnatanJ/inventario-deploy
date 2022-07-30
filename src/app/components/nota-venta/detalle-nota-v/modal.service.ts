70a9a6af74d99cb7e0eba75aae79b2e58dfecad8
Esther-Carrion <gatescc@hotmail.com>
2022-07-30 06:32:14 -0500

    vista ventas y lista reservas.

---
+++ src/app/components/nota-venta/detalle-nota-v/modal.service.ts
@@ -0,0 +1,21 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class modalService {
+
+  modal:Boolean=false;
+  constructor() { }
+
+  openModal(){
+    this.modal=true;
+  }
+
+  cerrarModal(){
+    this.modal=false;
+  }
+
+}

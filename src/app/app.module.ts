13b90a508b033b936f11119d48c5d746913ae407
Jhonnatan Bonilla <javiermarquez_99@hotmail.com>
2022-08-08 14:08:01 -0500

    generar PDF de reporte diario

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -44,6 +44,8 @@ import { BReservaFechaPipe } from './Pipe/b-reserva-fecha.pipe';
 import { DetalleReservaComponent } from './components/list-reserva/detalle-reserva/detalle-reserva.component';
 import { ReporteDiarioComponent } from './components/reporte-diario/reporte-diario.component';
 import { ReportePersonalizadoComponent } from './components/reporte-personalizado/reporte-personalizado.component';
+import * as pdfMake from 'pdfmake/build/pdfmake';
+import * as pdfFonts from 'pdfmake/build/vfs_fonts';
 
 @NgModule({
   declarations: [
@@ -76,8 +78,7 @@ import { ReportePersonalizadoComponent } from './components/reporte-personalizad
     BReservaFechaPipe,
     DetalleReservaComponent,
     ReporteDiarioComponent,
-    ReportePersonalizadoComponent,
-    
+    ReportePersonalizadoComponent,    
 
   ],
   imports: [
@@ -93,8 +94,7 @@ import { ReportePersonalizadoComponent } from './components/reporte-personalizad
     MatAutocompleteModule,
     MatInputModule,
     MatFormFieldModule,
-    NgxSpinnerModule ,
-    
+    NgxSpinnerModule
   ],
   providers: [],
   bootstrap: [AppComponent],
--- src/app/components/reporte-diario/reporte-diario.component.ts
+++ src/app/components/reporte-diario/reporte-diario.component.ts
@@ -4,6 +4,10 @@ import { ReporteService } from '../../services/reporte.service';
 import { NotaVenta } from '../../models/nota-venta';
 import { Reserva } from '../../models/reserva';
 
+import * as pdfMake from "pdfmake/build/pdfmake";
+import * as pdfFonts from 'pdfmake/build/vfs_fonts';
+(<any>pdfMake).vfs = pdfFonts.pdfMake.vfs;
+
 @Component({
   selector: 'app-reporte-diario',
   templateUrl: './reporte-diario.component.html',
@@ -94,4 +98,104 @@ export class ReporteDiarioComponent implements OnInit {
       this.padTo2Digits(date.getDate()),      
     ].join('-');
   }
+
+  //------------------------------------------------------------------------- GENERAR PDF CON PDF MAKER
+
+  createPdf(){
+    const pdfDefinition: any = {
+      info: {
+        title: `Reporte Diario_${this.fechaActual}`,
+        author: `${this.cuenta.usuario.nombres} + ' ' + ${this.cuenta.usuario.apellidos}`,
+        subject: 'Reporte de Ventas',
+        keywords: 'Reporte',
+        creator: 'Libreria GeoMundo'
+        
+        },
+      content: [
+        {
+          text: `Fecha: ${new Date().toLocaleString()}`,
+          alignment: 'right',
+        },
+      {
+        text: 'Libreria GEOMUNDO',
+        fontSize: 16,
+        alignment: 'start',          
+        color: '#2d8fd4',
+        bold: true, 
+        margin: [0, 0, 0, 20]     
+      },          
+      {
+        text: 'Reporte de Ingresos Diario',
+        fontSize: 16,                   
+        alignment: 'center',
+        margin:[0,0,0,20]
+      },
+      {
+        text: 'DATOS:',
+        style: 'sectionHeader'
+      },
+        {
+          text: `Generado Por: ${this.cuenta.usuario.nombres + ' ' + this.cuenta.usuario.apellidos}`,
+          margin:[0,0,0,5]
+        },
+        {
+          text: `Email: ${this.cuenta.email}`,
+          margin: [0,0,0,15],
+        },
+      {
+        text: 'INGRESOS:',
+        style: 'sectionHeader'
+      }, 
+      {
+        text: 'Notas de Venta'
+      },
+      {
+        table: {
+          headerRows: 1,
+          widths: ['auto', 'auto', '*', 'auto'],
+          body: [
+            ['ID', 'Vendedor', 'Libros', 'Valor Total'],
+            ...this.notasventa.map(notaventa => ([notaventa.idNotaventa, notaventa.cuenta.email, notaventa.detalles.map(detalle =>([`${'('+detalle.cantidad}`+')'+ `${detalle.libro.titulo}`])), {text:`$${notaventa.valorTotal.toFixed(2)}`, bold:true}])),
+            [{text: 'Total de Ventas Diario', colSpan: 3, bold:true},{},{},{text:`$${this.totalVentasDiario.toFixed(2)}`, bold:true}]
+          ]
+        }
+      },
+      {
+        text: 'Reservas',
+        margin:[0,20,0,0],
+      },
+      {
+        table: {
+          headerRows: 1,
+          widths: ['auto', 'auto', 'auto', '*', 'auto', 'auto', 'auto'],
+          body: [
+            ['ID', 'Vendedor', 'Cliente', 'Libros', 'Abono', 'Saldo', 'Valor Total'],
+            ...this.reservas.map(reserva => ([reserva.idReserva, reserva.cuenta.email, reserva.usuario.ci, reserva.detalleReservas.map(detalle =>([`${'('+detalle.cantidad}`+')'+ `${detalle.libro.titulo}`])), {text: `$${reserva.abono.toFixed(2)}`, bold:true}, {text:`$${reserva.saldo.toFixed(2)}`}, {text:`$${reserva.valorTotal.toFixed(2)}`}])),
+            [{text: 'Total de Abonos Diario', colSpan: 4, bold:true}, {},{},{}, {text:`$${this.totalAbonosDiario.toFixed(2)}`, bold:true},{},{},]
+          ],
+          margin:[0,0,0,20]
+        }
+      },
+      {
+        text: `TOTAL INGRESOS DIARIO: $${this.totalDiario.toFixed(2)}`,
+        style: 'sectionHeader',
+        margin:[0,20,0,0]
+      },      
+    ],
+    styles: {
+          sectionHeader: {
+            bold: true,
+            fontSize: 14,
+            margin: [0,0,0, 10]          
+          }
+      }
+    }
+
+    const pdf = pdfMake.createPdf(pdfDefinition);
+    pdf.open();
+    pdf.download();
+  }
+
+
+
 }

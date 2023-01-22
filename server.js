b5c479262eeaabdef4ec97442f07ec917ca62e9a
Jhonnatan Bonilla <javiermarquez_99@hotmail.com>
2023-01-22 15:36:01 -0500

    cambio nombre de app en dist

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const path = require('path');
 const app = express();
 
 //Serve only the static files form the dist directory
-app.use(express.static('./dist/invgeomundo'));
+app.use(express.static('./dist/inv_geomundo'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/invgeomundo/'}),
+    res.sendFile('index.html', {root: 'dist/inv_geomundo/'}),
 );
 
 //Start the app by listening on the default Heroku port
